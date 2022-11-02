d7b8ef3ac91ac66d26b22338a0f6f0080a2c930e
khader-zatari <khaderzatari@gmail.com>
2022-11-02 04:07:19 +0200

    change the main of VehiclesForAgency

--- src/main/java/StartAtGoogle/VehiclesForAgency/Main.java
+++ src/main/java/StartAtGoogle/VehiclesForAgency/Main.java
@@ -3,24 +3,21 @@ package StartAtGoogle.VehiclesForAgency;
 public class Main {
     public static void main(String[] args) {
         Passenger passenger1 = new Passenger("kahder", VehicleType.PLANE);
-        Passenger passenger2 = new Passenger("new Khader", VehicleType.BOAT);
+        Passenger passenger2 = new Passenger("Haitham", VehicleType.PLANE);
+        Passenger passenger3 = new Passenger("yazan", VehicleType.BOAT);
         Agency agency = new Agency(1, 4, 8, 3);
         agency.assignPassenger(passenger1);
         agency.assignPassenger(passenger2);
-        agency.assignPassenger(passenger1);
-        agency.assignPassenger(passenger1);
-        agency.assignPassenger(passenger1);
-        agency.assignPassenger(passenger1);
-        agency.assignPassenger(passenger1);
-        agency.assignPassenger(passenger1);
-        agency.assignPassenger(passenger1);
-        agency.assignPassenger(passenger1);
-        agency.assignPassenger(passenger1);
-        agency.assignPassenger(passenger1);
-        agency.assignPassenger(passenger1);
-        agency.assignPassenger(passenger1);
-        agency.assignPassenger(passenger1);
-        agency.assignPassenger(passenger1);
+        agency.assignPassenger(passenger3);
+
+//        agency.assignPassenger(passenger1);
+//        agency.assignPassenger(passenger1);
+//        agency.assignPassenger(passenger1);
+//        agency.assignPassenger(passenger1);
+//        agency.assignPassenger(passenger1);
+//        agency.assignPassenger(passenger1);
+//        agency.assignPassenger(passenger1);
+
 
 
         System.out.println(agency);
