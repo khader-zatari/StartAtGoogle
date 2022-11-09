fe1ffc014466c8edda9bc369dc0382122443808c
khader-zatari <khaderzatari@gmail.com>
2022-11-09 14:25:41 +0200

    use Log in repo class in the Authintication project

---
+++ src/main/java/StartAtGoogle/AuthenticationProject/Controllers/UserController.java
@@ -0,0 +1,94 @@
+package StartAtGoogle.AuthenticationProject.Controllers;
+
+import StartAtGoogle.AuthenticationProject.Services.*;
+import StartAtGoogle.AuthenticationProject.Services.UserService;
+
+import java.util.regex.*;
+
+public class UserController {
+    private static UserController userController;
+    private static UserService userService;
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile("^(?=.*[0-9])(?=.*[a-z]).{8,20}$");
+    private static final Pattern emailPattern = Pattern.compile(".+@.+\\.[a-z]+");
+    private static AuthenticationService authenticationService;
+
+
+    private UserController() {
+        userService = UserService.getInstance();
+        authenticationService = AuthenticationService.getInstance();
+    }
+
+    public static synchronized UserController getInstance() {
+        if (userController == null) {
+            userController = new UserController();
+        }
+        return userController;
+    }
+
+    public void updateUserName(String id, String token, String userName) {
+        if (authenticateUser(id, token)) {
+            userService.updateUserName(id, userName);
+        }
+        throw new IllegalStateException("The user was not authenticated");
+    }
+
+    public void updateEmail(String id, String token, String email) {
+        if (authenticateUser(id, token)) {
+            if (validateEmail(email)) {
+                userService.updateEmail(id, email);
+            } else {
+                throw new IllegalArgumentException("Invalid email inserted");
+            }
+        } else {
+            throw new IllegalStateException("The user was not authenticated");
+        }
+
+    }
+
+    public void updatePassword(String id, String token, String password) {
+        if (authenticateUser(id, token)) {
+            if (validatePassword(password)) {
+                userService.updatePassword(id, password);
+            } else {
+                throw new IllegalArgumentException("Invalid password inserted");
+            }
+        } else {
+            throw new IllegalStateException("The user was not authenticated");
+        }
+
+    }
+
+    public boolean authenticateUser(String id, String token) {
+        return authenticationService.authUser(id, token);
+    }
+
+    public boolean validateEmail(String email) {
+        Matcher m = emailPattern.matcher(email);
+        boolean matchFound = m.matches();
+        if (matchFound) {
+            if (userService.checkIfEmailExists(email)) {
+                throw new IllegalArgumentException("You cant change the email to the same one");
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public boolean validatePassword(String password) {
+        Matcher m = PASSWORD_PATTERN.matcher(password);
+        boolean matchFound = m.matches();
+        if (matchFound) {
+            return true;
+        }
+        return false;
+    }
+
+    public void deleteUser(String id, String token) {
+        if (authenticateUser(id, token)) {
+            authenticationService.deleteUserFromMap(id);
+            userService.deleteUser(id);
+        } else {
+            throw new IllegalStateException("The user was not authenticated");
+        }
+    }
+}
---
+++ src/main/java/StartAtGoogle/AuthenticationProject/Services/AuthenticationService.java
@@ -0,0 +1,93 @@
+package StartAtGoogle.AuthenticationProject.Services;
+
+import StartAtGoogle.AuthenticationProject.User;
+import StartAtGoogle.AuthenticationProject.UserRepository.UserRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class AuthenticationService {
+    private static Logger logger = LogManager.getLogger(AuthenticationService.class.getName());
+    private static AuthenticationService authenticationService;
+    private static UserRepository userRepository;
+
+    private AuthenticationService() {
+        userRepository = UserRepository.getInstance();
+    }
+
+    public static synchronized AuthenticationService getInstance() {
+        if (authenticationService == null) {
+            authenticationService = new AuthenticationService();
+        }
+        return authenticationService;
+    }
+
+    public static HashMap<String, String> userTokens = new HashMap<>();
+
+
+    public void createUser(String name, String email, String password) {
+        if (userRepository.checkIfEmailExists(email)) {
+            logger.error("the user has already registered");
+            throw new IllegalArgumentException("the user has already registered");
+        }
+        User user = new User(name, email, password);
+        userRepository.createUser(user);
+    }
+
+    public HashMap<String, String> logIn(String email, String password) {
+
+        String id;
+        if (userRepository.checkIfUserExists(email, password)) {
+            id = userRepository.getIdByEmail(email);
+        } else {
+            throw new IllegalArgumentException("the user is not valid");
+        }
+        if (userTokens.containsKey(id)) {
+            throw new IllegalArgumentException("the user is logged in ");
+        }
+
+        String token = createToken();
+        userTokens.put(id, token);
+        HashMap<String, String> res = new HashMap<>();
+        res.put(id, token);
+
+        return res;
+    }
+
+    public boolean authUser(String id, String token) {
+        for (HashMap.Entry<String, String> entry : userTokens.entrySet()) {
+            if (entry.getKey().equals(id)) {
+                return entry.getValue().equals(token);
+            }
+        }
+        return false;
+    }
+
+
+    public void deleteUserFromMap(String id) {
+        userTokens.remove(id);
+    }
+
+    private static String getSaltString(int stringLength) {
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        while (salt.length() < stringLength) {
+            int index = (int) (ThreadLocalRandom.current().nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+
+        return salt.toString();
+
+    }
+
+    public boolean checkIfEmailExists(String email) {
+        return userRepository.checkIfEmailExists(email);
+    }
+
+    public static String createToken() {
+        return getSaltString(18);
+    }
+
+}
